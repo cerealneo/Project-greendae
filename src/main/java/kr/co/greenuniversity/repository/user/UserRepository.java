55e75e6e1e8f92370716afe5ca92e2954c950dab
minsung <dpsdy1@gmail.com>
2025-03-31 17:38:27 +0900

    add

--- src/main/java/kr/co/greenuniversity/repository/user/UserRepository.java
+++ src/main/java/kr/co/greenuniversity/repository/user/UserRepository.java
@@ -4,10 +4,14 @@ import kr.co.greenuniversity.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     long countById(String id);
     long countByEmail(String email);
     long countByPhone(String phone);
+
+    Optional<User> findByEmail(String email);
 }
