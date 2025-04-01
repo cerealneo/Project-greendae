3ba54feef14596977ce14baf3e6f188ca17be995
minsung <dpsdy1@gmail.com>
2025-04-01 17:08:39 +0900

    add

--- src/main/java/kr/co/greenuniversity/controller/BachelorController.java
+++ src/main/java/kr/co/greenuniversity/controller/BachelorController.java
@@ -1,45 +1,48 @@
 package kr.co.greenuniversity.controller;
 
+import kr.co.greenuniversity.entity.Bachelor.Calender;
+import kr.co.greenuniversity.service.Bachelor.CalenderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/Bachelor")
 public class BachelorController {
 
-    @GetMapping("/Bachelor/BacGrad")
-    public String BacGrad () {
+    // ✅ 학사 일정 페이지 반환 (HTML View)
+    @GetMapping("/BacGrad")
+    public String BacGrad() {
         return "/Bachelor/BacGrad";
     }
 
-    @GetMapping("/Bachelor/BacNotice")
-    public String BacNotice () {
+    @GetMapping("/BacNotice")
+    public String BacNotice() {
         return "/Bachelor/BacNotice";
     }
 
-    @GetMapping("/Bachelor/BacQuestion")
-    public String BacQuestion () {
+    @GetMapping("/BacQuestion")
+    public String BacQuestion() {
         return "/Bachelor/BacQuestion";
     }
 
-    @GetMapping("/Bachelor/BacResult")
-    public String BacResult () {
+    @GetMapping("/BacResult")
+    public String BacResult() {
         return "/Bachelor/BacResult";
     }
 
-    @GetMapping("/Bachelor/BacSchedule")
-    public String BacSchedule () {
+    @GetMapping("/BacSchedule")
+    public String BacSchedule() {
         return "/Bachelor/BacSchedule";
     }
 
-    @GetMapping("/Bachelor/BacSubscribe")
-    public String BacSubscribe () {
+    @GetMapping("/BacSubscribe")
+    public String BacSubscribe() {
         return "/Bachelor/BacSubscribe";
     }
-
-
-
 }
