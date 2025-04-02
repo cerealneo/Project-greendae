cba9fda1d2c7264523e995549914accefbc8ac5d
seongmin-01 <fafa5271783@gmail.com>
2025-04-02 12:53:09 +0900

    아이디 찾기 조금 수정

--- src/main/java/kr/co/greenuniversity/controller/user/FindController.java
+++ src/main/java/kr/co/greenuniversity/controller/user/FindController.java
@@ -8,12 +8,10 @@ import kr.co.greenuniversity.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +24,7 @@ public class FindController {
 
     private final UserService userService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/findId")
     public String findId() {
@@ -35,30 +34,61 @@ public class FindController {
 
     @PostMapping("/findId")
     public String findId(UserDTO userDTO, Model model) {
-        //User user = userService.findUserId(userDTO);
 
-        /*if (user != null) {
-            model.addAttribute("userName", user.getId());
-            return "/user/ResultId"; // 결과 페이지로 이동
-        } else {
-            model.addAttribute("errorMessage", "입력한 이메일과 일치하는 아이디가 없습니다.");
-            return "/user/findId"; // 다시 입력 폼으로 이동
-        }*/
+      User findUser = userService.findUserId(userDTO);
 
-        Optional<User> optUser = userRepository.findByEmail(userDTO.getEmail());
+      if(findUser != null) {
+          log.info("찾은 유저: {}", findUser);
 
-        if (optUser.isPresent()) {
-            User user = optUser.get();
-            log.info("찾은 유저: {}", user);
+          model.addAttribute("resultId", findUser.getId());
 
-            model.addAttribute("foundUserId", user.getId()); // ID 값을 모델에 추가
-            return "/user/resultId"; // 결과 페이지로 이동
-        }
-
-        model.addAttribute("error", "해당 이메일로 등록된 계정이 없습니다.");
-        return "/user/findId"; // 이메일이 없으면 다시 폼으로 이동
+          return "/user/ResultId";
+      }
+      log.info("사용자 없음");
+      model.addAttribute("error", "해당 사용자를 찾을 수 없습니다.");
+      return "/user/ResultId";
     }
 
+    @GetMapping("/findPass")
+    public String findPass() {
+        return "/user/findPass";
+    }
+
+    @PostMapping("/findPass")
+    public String findPass(UserDTO userDTO, Model model) {
+        User findUser = userService.findUserId(userDTO);
+
+        if(findUser != null) {
+            log.info("찾은 유저: {}", findUser);
+
+            model.addAttribute("resultId", findUser.getId());
+
+            return "/user/updatePass";
+        }
+        log.info("사용자 없음");
+        model.addAttribute("error", "해당 사용자를 찾을 수 없습니다.");
+        return "/user/ResultId";
+    }
+
+    // 새 비밀번호 설정
+    @GetMapping("/updatePass")
+    public String updatePass() {
+
+        return "/user/updatePass";
+    }
+
+    // 새 비밀번호 설정
+   /* @PostMapping("/updatePass")
+    public String updatePass(@RequestParam("id") String id, @RequestParam("password") String password) {
+        log.info("id: {}", id);
+        log.info("password: {}", password);
+
+        userService.updatePass(id, password);
+
+        return "/user/updatePass";
+    } */
+
+    // 이메일 체크
     @GetMapping("/find/{type}/{value}")
     public ResponseEntity user(@PathVariable("type") String type, @PathVariable("value") String value){
         log.info("type : " + type + ", value : " + value);
@@ -76,6 +106,7 @@ public class FindController {
         return ResponseEntity.ok().body(resultMap);
     }
 
+    // 이메일 발송
     @PostMapping("/find/email/auth")
     public ResponseEntity<Boolean> emailAuth(@RequestBody Map<String,String> map, HttpSession session){ // JSON 단일 문자열값이 직접 String으로 매핑되지 않기 때문에 JSON과 호환되는 Map 타입으로 JSON 수신
 
