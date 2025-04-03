2b8b1aa5e30c39b3ebf834a20e59f43ff197b3f4
seongmin-01 <fafa5271783@gmail.com>
2025-04-03 10:51:59 +0900

    학생목록 하는중

--- src/main/java/kr/co/greenuniversity/repository/community/Community2Repository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.greenuniversity.repository.community;
-
-import kr.co.greenuniversity.entity.community.Community2;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Community2Repository extends JpaRepository<Community2, Integer>, kr.co.greenuniversity.repository.custom.Community2RepositoryCustom {
-}
--- src/main/java/kr/co/greenuniversity/repository/user/TermsRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.greenuniversity.repository.user;
-
-import kr.co.greenuniversity.entity.user.Terms;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TermsRepository extends JpaRepository<Terms, Integer> {
-}
